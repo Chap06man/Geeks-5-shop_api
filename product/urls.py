2c9297239a2161ef4e64dfd5d78e2becc3986772
Sardar <sardorbeksattarov4@gmail.com>
2026-08-28 09:23:11 +0000

    Homework-3

--- product/urls.py
+++ product/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/v1/categories/',views.list_categories_api_view),
+    path('api/v1/categories/',views.list_categories_create_api_view),
     path('api/v1/categories/<int:id>/', views.deatil_categories_api_view),
-    path('api/v1/products/', views.list_product_api_view),
+    path('api/v1/products/', views.list_product_create_api_view),
     path('api/v1/products/<int:id>/', views.detail_prod_api_view),
-    path('api/v1/reviews/', views.list_review_api_view),
+    path('api/v1/reviews/', views.list_review_create_api_view),
     path('api/v1/reviews/<int:id>/', views.detail_review_api_view),
     path('api/v1/product/reviews/', views.prod_review_list_api_views),
 ]
--- product/views.py
+++ product/views.py
@@ -8,67 +8,152 @@ from . serializer import CatySeriaList,CatySeriaDetail, ProdSeriaList,ProdSeriaD
 #1-Homework
 
 #list,Detail - Category 
-@api_view(['GET'])
-def list_categories_api_view(request):
-    category = Category.objects.all()
-    list_ = CatySeriaList(category, many=True).data
-    return Response(
-        status=status.HTTP_200_OK,
-        data = list_
-    )
-@api_view(['GET'])
+@api_view(['GET','POST'])
+def list_categories_create_api_view(request):
+    if request.method == 'GET':
+        category = Category.objects.all()
+        list_ = CatySeriaList(category, many=True).data
+        return Response(
+            status=status.HTTP_200_OK,
+            data = list_
+        )
+    #Homework-3/---------------------------------------->
+    if request.method == 'POST':
+        name = request.data.get('name')
+        caty = Category.objects.create(name=name)
+        
+    return Response(data=CatySeriaList(caty).data,
+                        status=status.HTTP_201_CREATED)
+    #--------------------------------------------------->
+
+@api_view(['GET','PUT','DELETE'])
 def deatil_categories_api_view(request, id):
     try:
         caty = Category.objects.get(id=id)
     except Category.DoesNotExist:
         return Response(data={'error':'Not found!!!'},
                         status=status.HTTP_404_NOT_FOUND)
-         
-    category = Category.objects.get(id=id)
-    deatil = CatySeriaDetail(category, many=False).data
-    return Response(data=deatil)
+
+    #Homework-3/------------------------------------------------>
+    if request.method == 'GET':   
+        category = Category.objects.get(id=id)
+        deatil = CatySeriaDetail(category, many=False).data
+        return Response(data=deatil)
+    
+    elif request.method =='DELETE':
+        caty.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == "PUT":
+        caty.name = request.data.get('name')
+        caty.save()
+        return Response(status=status.HTTP_201_CREATED,
+                        data=CatySeriaDetail(caty).data)
+    #-------------------------------------------------------------->
 
 #List,Detail - Product
-@api_view(['GET'])
-def list_product_api_view(request):
-    product = Product.objects.all()
-    list_ = ProdSeriaList(product, many=True).data
-    return Response(
-        status=status.HTTP_200_OK,
-        data=list_
-    )
+@api_view(['GET', 'POST'])
+def list_product_create_api_view(request):
 
-@api_view(['GET'])
+    if request.method == 'GET':
+        product = Product.objects.all()
+        list_ = ProdSeriaList(product, many=True).data
+        return Response(
+            status=status.HTTP_200_OK,
+            data=list_
+        )
+
+    #Homework-3/-------------------------------------------------->
+    elif request.method == "POST":
+        title = request.data.get('title')
+        descriptions = request.data.get('descriptions')
+        price = request.data.get('price')
+        category_id = request.data.get('category_id')
+
+        prod = Product.objects.create(
+            title = title,
+            descriptions = descriptions,
+            price = price,
+            category_id = category_id
+        )
+        return Response(data=ProdSeriaList(prod).data,
+                        status=status.HTTP_201_CREATED)
+    #-------------------------------------------------------------->
+
+@api_view(['GET','PUT','DELETE'])
 def detail_prod_api_view(request, id):
     try:
         prod = Product.objects.get(id=id)
     except Product.DoesNotExist:
            return Response(data={'error':'Not found!!!'},
                            status=status.HTTP_404_NOT_FOUND)
-    product = Product.objects.get(id=id)
-    deatil = ProdSeriaDetail(product, many=False).data
-    return Response(data=deatil)      
+
+    #Homework-3/-------------------------------------------------->
+    if request.method == "GET":
+        product = Product.objects.get(id=id)
+        deatil = ProdSeriaDetail(product, many=False).data
+        return Response(data=deatil) 
+    
+    elif request.method =='DELETE':
+        prod.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == "PUT":
+        prod.title = request.data.get('title')
+        prod.descriptions = request.data.get('descriptions')
+        prod.price = request.data.get('price')
+        prod.category_id = request.data.get('category')
+        prod.save()
+        return Response(status=status.HTTP_201_CREATED,
+                        data=ProdSeriaDetail(prod).data)     
+    #-------------------------------------------------------------------->
 
 #List, Detail - Review
-@api_view(['GET'])
-def list_review_api_view(request):
-    review = Review.objects.all()
-    list_ = ReviewSeriaList(review, many=True).data
-    return Response(
-        status=status.HTTP_200_OK,
-        data=list_
-    )
+@api_view(['GET','POST'])
+def list_review_create_api_view(request):
+    if request.method == 'GET':
+        review = Review.objects.all()
+        list_ = ReviewSeriaList(review, many=True).data
+        return Response(
+            status=status.HTTP_200_OK,
+            data=list_
+        )
+    #Homework-3/------------------------------------------------->
+    elif request.method == 'POST':
+        text = request.data.get('text')
+        product_id = request.data.get('product')
+        stars = request.data.get('stars')
 
-@api_view(['GET'])
+        rev = Review.objects.create(
+            text=text,
+            product_id=product_id,
+            stars=stars
+        )
+        return Response(data=ReviewSeriaList(rev).data,
+                        status=status.HTTP_201_CREATED)
+    #--------------------------------------------------------------->
+        
+@api_view(['GET','PUT','DELETE'])
 def detail_review_api_view(request, id):
     try:
         rev = Review.objects.get(id=id)
     except Review.DoesNotExist:
            return Response(data={'error':'Not found!!!'},
                            status=status.HTTP_404_NOT_FOUND)
-    review = Review.objects.get(id=id)
-    deatil = ReviewSeriaDetail(review, many=False).data
-    return Response(data=deatil) 
+    #Homework-3/------------------------------------------------->
+    if request.method == 'GET':
+        review = Review.objects.get(id=id)
+        deatil = ReviewSeriaDetail(review, many=False).data
+        return Response(data=deatil) 
+    elif request.method == 'DELETE':
+        rev.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PUT':
+        rev.text = request.data.get('text')
+        rev.product_id = request.data.get('product')
+        rev.stars = request.data.get('stars')
+        rev.save()
+        return Response(status=status.HTTP_201_CREATED,
+                        data=ReviewSeriaDetail(rev).data)
+    #------------------------------------------------------------>
 
 #Homework-2 
 @api_view(['GET'])
